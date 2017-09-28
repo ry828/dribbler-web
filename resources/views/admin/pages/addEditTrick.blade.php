faeedd1dcde8c2b2928bbc2b2ba8696b7fd2e65e
Admin <admin@Mac-Admin.local>
2017-09-28 12:18:52 +0600

    added dynamic description input box in trick edit/add

--- resources/views/admin/pages/addEditTrick.blade.php
+++ resources/views/admin/pages/addEditTrick.blade.php
@@ -13,10 +13,91 @@
     <script type="text/javascript" src="{{ URL::asset('admin_assets/js/pages/form_inputs.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('admin_assets/js/pages/components_modals.js') }}"></script>
 
+
+    <script>
+        $(function () {
+            var description_count = 0;
+            var description_text = "{{isset($trick) ? $trick->trick_description : ''}}";
+            description_text = description_text.replace(/&quot;/g, '\"');
+
+
+            $("#btn_add_description").on("click", function () {
+                add_description_box("", "", "");
+                init_inputbox_style();
+            });
+            function add_description_box(title, thumbnail, description) {
+               html = '';
+               html += '<ul style="list-style-type: none">' +
+                       '<li style="padding: 8px">' +
+                       '<div class="form-group">\n' +
+                       '    <label class="control-label col-lg-2"> Title</label>\n' +
+                       '    <div class="col-lg-10">\n' +
+                       '        <input type="text" class="form-control" placeholder="Title" name="title[]" value="'+title+'" required>\n' +
+                       '    </div>\n' +
+                       '</div>' +
+                       '</li>' +
+                       '<li style="padding: 8px">' +
+                       '<div class="form-group">\n' +
+                        '    <label class="control-label col-lg-2">Photo</label>\n' +
+                        '    <div class="col-lg-10">\n' +
+                        '        <input type="file" class="description-file-input form-control" name="picture[]" accept=".png, .jpg" data-allowed-file-extensions=\'["png", "jpg"]\' data-show-caption="true" required>\n' +
+                        '    </div>\n' +
+                        '</div>' +
+                        '</li>' +
+                       '<li style="padding: 8px">' +
+                       '<div class="form-group">\n' +
+                        '    <label class="control-label col-lg-2"> Description</label>\n' +
+                        '    <div class="col-lg-10">\n' +
+                        '        <textarea class="form-control" placeholder="Description" name="description[]" value="'+description+'" required>' + description + '</textarea>\n' +
+                        '    </div>\n' +
+                        '</div>' +
+                        '</li>' +
+                        '<li style="padding: 12px">' +
+                        '<a class="delete_description" style="float:right">Delete</a>' +
+                        '</li>' +
+
+                    '</ul>';
+               $('#div_description').append(html);
+               $("#div_description").css('display', '');
+               description_count ++;
+
+           }
+           $("#div_description").on("click", ".delete_description", function () {
+               if (description_count > 1) {
+                   $(this).parent().parent().remove();
+                   description_count --;
+               }
+
+           });
+            function init_inputbox_style() {
+                $('.description-file-input').fileinput({
+                    browseLabel: 'Browse',
+                    browseIcon: '<i class="icon-file-plus"></i>',
+                    uploadIcon: '<i class="icon-file-upload2"></i>',
+                    removeIcon: '<i class="icon-cross3"></i>',
+                    showUpload: false,
+                    layoutTemplates: {
+                        icon: '<i class="icon-file-check"></i>'
+                    },
+                    initialCaption: "No file selected"
+                });
+            }
+            if (description_text != '') {
+                var arr_description = JSON.parse(description_text);
+                for (var i in arr_description) {
+                    add_description_box(arr_description[i].title,arr_description[i].thumbnail, arr_description[i].description);
+                }
+
+            } else {
+                add_description_box("", "", "");
+            }
+
+
+        });
+    </script>
     <script type="text/javascript">
         $(function(){
-            // Basic example
-            $('.file-input').fileinput({
+            $('.file-input, .description-file-input').fileinput({
                 browseLabel: 'Browse',
                 browseIcon: '<i class="icon-file-plus"></i>',
                 uploadIcon: '<i class="icon-file-upload2"></i>',
@@ -27,6 +108,7 @@
                 },
                 initialCaption: "No file selected"
             });
+
         });
     </script>
 @endsection
@@ -87,20 +169,25 @@
                         <div class="form-group" hidden="true">
                             <label class="control-label col-lg-2"> Trick ID</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" placeholder="trick title" name="trick_id" value="{{isset($trick) ? $trick->trick_id : '0'}}" required>
+                                <input type="text" class="form-control" placeholder="" name="trick_id" value="{{isset($trick) ? $trick->trick_id : '0'}}" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label col-lg-2"> Trick title</label>
                             <div class="col-lg-10">
-                                <input type="text" class="form-control" placeholder="trick title" name="trick_title" value="{{isset($trick) ? $trick->trick_title : ''}}" required>
+                                <input type="text" class="form-control" placeholder="Trick title" name="trick_title" value="{{isset($trick) ? $trick->trick_title : ''}}" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="control-label col-lg-2"> Description</label>
                             <div class="col-lg-10">
-                                <input type="text" placeholder="description" class="form-control" name="trick_description" value="{{isset($trick) ? $trick->trick_describtion : ''}}" required>
+
+                                {{--<input type="text" placeholder="description" class="form-control" name="trick_description" value="{{isset($trick) ? $trick->trick_describtion : ''}}" required>--}}
+                                <div class="panel" id="div_description">
+
+                                </div>
+                                <button type="button" class="btn btn-default" id="btn_add_description"><i class="icon-plus3 position-left"></i> Add</button>
                             </div>
                         </div>
 
@@ -118,7 +205,7 @@
                         <div class="form-group">
                             <label class="control-label col-lg-2">Trick tags</label>
                             <div class="col-lg-10">
-                                <select multiple="multiple" class="select" name="trick_tags" data-placeholder="select tags" required>
+                                <select multiple="multiple" class="select" name="trick_tags" data-placeholder="Select tags" required>
                                     @if (isset($trick))
                                         @foreach($tags as $tag)
                                             <option value="{{$tag->tag_name}}"
